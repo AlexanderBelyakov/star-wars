0d34f93177b674b1ecb91c7ced037af2deca63d6
Alexander Belyakov <belyakov-alexander-2017@yandex.ru>
2024-02-26 22:33:24 +0300

    feat: correct project structure

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img className="App-logo" alt="logo" />
+        <img  className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
